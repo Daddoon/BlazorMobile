bedd7f30e2b42969a31c3d4e2385158a9085606d
Daddoon <guillaume.zahra@daddoon.com>
2018-04-29 20:00:50 +0200

    Additional test. Issue with local content serving.
    Additional test. Issue with local content serving. Will look for a workaround with EmbedIO

--- src/Daddoon.Blazor.Xam.Template/Daddoon.Blazor.Xam.Template/MainPage.xaml.cs
+++ src/Daddoon.Blazor.Xam.Template/Daddoon.Blazor.Xam.Template/MainPage.xaml.cs
@@ -16,22 +16,24 @@ namespace Daddoon.Blazor.Xam.Template
 
             //Note: How to manage local files: https://docs.microsoft.com/fr-fr/xamarin/xamarin-forms/user-interface/webview?tabs=vswin
 
+            //TOOD: Read about EmbedIO for a embed webserver: https://spin.atomicobject.com/2018/01/08/xamarin-forms-embedded-web-server/
+
             var source = new HtmlWebViewSource();
             source.BaseUrl = DependencyService.Get<IBaseUrl>().Get();
-            source.Html = @"<!DOCTYPE html>
+            source.Html = $@"<!DOCTYPE html>
                             <html>
                             <head>
                                 <meta charset=""utf-8"" />
                                 <title>Daddoon.Blazor.Xam.BlazorApp</title>
-                                <base href=""/"" />
+                                <base href=""{source.BaseUrl}"" />
                                 <link href=""css/bootstrap/bootstrap.min.css"" rel=""stylesheet"" />
                                 <link href=""css/site.css"" rel=""stylesheet"" />
                             </head>
                             <body>
                                 <app>Loading...</app>
 
-                                <script src=""css/bootstrap/bootstrap-native.min.js""></script>
-                                <script src=""_framework/blazor.js"" main=""Daddoon.Blazor.Xam.BlazorApp.dll"" entrypoint=""Daddoon.Blazor.Xam.BlazorApp.Program::Main"" references=""Microsoft.AspNetCore.Blazor.Browser.dll,Microsoft.AspNetCore.Blazor.dll,Microsoft.Extensions.DependencyInjection.Abstractions.dll,Microsoft.Extensions.DependencyInjection.dll,mscorlib.dll,netstandard.dll,System.Core.dll,System.dll,System.Net.Http.dll"" linker-enabled=""true""></script>
+                                <script type=""text/javascript"" src=""css/bootstrap/bootstrap-native.min.js""></script>
+                                <script type=""text/javascript"" src=""_framework/blazor.js"" main=""Daddoon.Blazor.Xam.BlazorApp.dll"" entrypoint=""Daddoon.Blazor.Xam.BlazorApp.Program::Main"" references=""Microsoft.AspNetCore.Blazor.Browser.dll,Microsoft.AspNetCore.Blazor.dll,Microsoft.Extensions.DependencyInjection.Abstractions.dll,Microsoft.Extensions.DependencyInjection.dll,mscorlib.dll,netstandard.dll,System.Core.dll,System.dll,System.Net.Http.dll"" linker-enabled=""true""></script>
                             </body>
                             </html>
                             ";
@@ -45,6 +47,7 @@ namespace Daddoon.Blazor.Xam.Template
                 Source = source
             };
             content.Children.Add(webview);
+            
 		}
 	}
 }
