9adfc747d5a1ef33343931795591bf1301ffe45f
Guillaume <gzahra@2bee.io>
2019-08-04 11:30:59 +0200

    - Added UWP projects

---
+++ src/BlazorMobile.UWP/Services/BlazorWebViewService.cs
@@ -0,0 +1,34 @@
+﻿using BlazorMobile.UWP.Renderer;
+using BlazorMobile.Services;
+using System;
+using System.IO;
+using Xamarin.Forms;
+using BlazorMobile.Common.Interfaces;
+
+namespace BlazorMobile.UWP.Services
+{
+    public class BlazorWebViewService
+    {
+        private static void InitComponent()
+        {
+            BlazorWebViewRenderer.Init();
+        }
+
+        /// <summary>
+        /// Init the WebApplicationFactory with the given app stream resolver.
+        /// Shorthand for <see cref="WebApplicationFactory.RegisterAppStreamResolver" />
+        /// </summary>
+        /// <param name="appStreamResolver"></param>
+        public static void Init(Func<Stream> appStreamResolver)
+        {
+            InitComponent();
+            WebApplicationFactory.Init(appStreamResolver);
+        }
+
+        public static void Init()
+        {
+            InitComponent();
+            WebApplicationFactory.Init();
+        }
+    }
+}
