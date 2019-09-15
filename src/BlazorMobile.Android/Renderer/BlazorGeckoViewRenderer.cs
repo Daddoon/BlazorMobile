04bb531dcdf035aaeb856c5a99239993b0a65206
Guillaume <gzahra@2bee.io>
2019-09-15 13:40:53 +0200

    - Created NuGet package BlazorMobile.Build.Android - GeckoView Iframe Listener extension is now injected as an asset at build time of Android project through BlazorMobile.Build.Android NuGet Package - Removed files in project reference to Iframe Listener web extension - Added NuGet package in BlazorMobile.Sample.Android

--- src/BlazorMobile.Android/Renderer/BlazorGeckoViewRenderer.cs
+++ src/BlazorMobile.Android/Renderer/BlazorGeckoViewRenderer.cs
@@ -36,7 +36,7 @@ namespace BlazorMobile.Droid.Renderer
             //This asset must be added in the developer project manually or injected at build if not present through a MSBuild task
             //This is needed because Android does not support sharing asset from class library projects, so we can't reference it directly from BlazorMobile.Android
             //NOTE: Delegate for WebExtension handling seem missing from current Xamarin.GeckoView generated bindings, but the handling will be workarounded through the local BlazorMobile server
-            _runtime.RegisterWebExtension(new WebExtension("resource://android/assets/BlazorMobile/web_extensions/iframe_listener/", "BlazorMobileIframeListener"));
+            _runtime.RegisterWebExtension(new WebExtension("resource://android/assets/obj/BlazorMobile/web_extensions/iframe_listener/", "BlazorMobileIframeListener"));
 
             _session.Open(_runtime);
             _session.ProgressDelegate = new BlazorProgressDelegate(this);
