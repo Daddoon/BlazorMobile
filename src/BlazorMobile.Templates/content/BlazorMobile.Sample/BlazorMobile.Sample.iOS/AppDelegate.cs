03e85cc53d5efd8bbd738472a009a9290519e8be
Guillaume <gzahra@2bee.io>
2019-10-06 18:05:32 +0200

    - Added forgotten iOS patch in sample

--- src/BlazorMobile.Templates/content/BlazorMobile.Sample/BlazorMobile.Sample.iOS/AppDelegate.cs
+++ src/BlazorMobile.Templates/content/BlazorMobile.Sample/BlazorMobile.Sample.iOS/AppDelegate.cs
@@ -27,6 +27,11 @@ namespace BlazorMobile.Sample.iOS
             //Register our Blazor app package
             WebApplicationFactory.RegisterAppStreamResolver(AppPackageHelper.ResolveAppPackageStream);
 
+            if (int.TryParse(UIDevice.CurrentDevice.SystemVersion.Split('.')[0], out int majorVersion) && majorVersion >= 13)
+            {
+                BlazorWebViewService.EnableDelayedStartPatch();
+            }
+
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
