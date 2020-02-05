6334689b5a312a2e65b4e419e9674fa5d276189b
Guillaume <gzahra@2bee.io>
2020-02-05 13:28:42 +0100

    - Updated BlazorMobile.Sample to new format

--- src/BlazorMobile.Templates/content/BlazorMobile.Sample/BlazorMobile.Sample.Blazor/Program.cs
+++ src/BlazorMobile.Templates/content/BlazorMobile.Sample/BlazorMobile.Sample.Blazor/Program.cs
@@ -1,16 +1,40 @@
-﻿using Microsoft.AspNetCore.Blazor.Hosting;
+﻿using BlazorMobile.Common;
+using BlazorMobile.Common.Services;
+using BlazorMobile.Sample.Blazor.Helpers;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using System;
+using System.Threading.Tasks;
 
 namespace BlazorMobile.Sample.Blazor
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
-            BlazorWebAssemblyHost.CreateDefaultBuilder()
-                .UseBlazorStartup<Startup>();
+            #region Services registration
+
+            ServicesHelper.ConfigureCommonServices(builder.Services);
+
+            #endregion
+
+            #region DEBUG
+
+            //Only if you want to test WebAssembly with remote debugging from a dev machine
+            BlazorMobileService.EnableClientToDeviceRemoteDebugging("127.0.0.1", 8888);
+
+            #endregion
+
+            BlazorMobileService.Init((bool success) =>
+            {
+                Console.WriteLine($"Initialization success: {success}");
+                Console.WriteLine("Device is: " + BlazorDevice.RuntimePlatform);
+            });
+
+            builder.RootComponents.Add<MobileApp>("app");
+
+            await builder.Build().RunAsync();
+        }
     }
 }
