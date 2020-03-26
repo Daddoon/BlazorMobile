511deda55e68a0dd45680bc63dda0e0cec9cc2e5
Guillaume <gzahra@2bee.io>
2020-03-26 19:16:53 +0100

    - Added  file input + an image loaded from file input in web page for testing

---
+++ src/BlazorMobile.InteropBlazorApp/wwwroot/test.js
@@ -0,0 +1,22 @@
+﻿
+window.ApplyOnChangeEventOnFileSelector = function () {
+    document.getElementById('picField').onchange = function (evt) {
+        var tgt = evt.target || window.event.srcElement,
+            files = tgt.files;
+
+        // FileReader support
+        if (FileReader && files && files.length) {
+            var fr = new FileReader();
+            fr.onload = function () {
+                document.getElementById("outImage").src = fr.result;
+            }
+            fr.readAsDataURL(files[0]);
+        }
+
+        // Not supported
+        else {
+            // fallback -- perhaps submit the input to an iframe and temporarily store
+            // them on the server until the user's session ends.
+        }
+    }
+};
